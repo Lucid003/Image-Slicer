003b01a665eb4a85d83198c41edf9639b95360a2
Lucid003 <68939550+Lucid003@users.noreply.github.com>
2020-10-21 16:27:38 -0500

    Update imagesplit.py

--- imagesplit.py
+++ imagesplit.py
@@ -13,14 +13,15 @@ for image in entries:
     toProcess.apend(Image.open(images))'''
 
 print('Enter your desired tile size in pixels (Example: 16 will be a 16x16 tile):')
+while True:
+ try:
+  tilesize = int(input('Enter your desired tile size in pixels (Example: 16 will be a 16x16 tile):'))
+  break
+ except:
+  print("You must enter an integer!")
+  
 tilesize = input()
 tilesize = int(tilesize)
-
-rows = im.size[0] / tilesize
-cols = im.size[1] / tilesize
-
-ir = 0
-ic = 0
 
 with os.scandir(/') as entries:
     for entry in entries:
@@ -31,7 +32,7 @@ with os.scandir(/') as entries:
         cols = math.ceil(cols)
         for x in range(rows):
                 for y in range(cols):
-                      box = (0 + ic * tilesize, 0 + ir * tilesize, tilesize + ic * tilesize, tilesize + ir * tilesize)
+                      box = (0 + y * tilesize, 0 + x * tilesize, tilesize + y * tilesize, tilesize + x * tilesize)
                       region = im.crop(box)
                       slice = Image.new('RGBA', (tilesize, tilesize), color=255)
                       mask=Image.new('L', slice.size, color=255)
@@ -39,8 +40,10 @@ with os.scandir(/') as entries:
                       draw.rectangle((0,0,tilesize,tilesize), fill=0)
                       slice.putalpha(mask)
                       slice.paste(region, (0,0))
+                      dirname = entry.rstrip('.png')
+                      os.mkdir(dirname)
                       outfile = "r" + str(ir+1) + "c" + str(ic+1) + ".png"
-                      slice.save(outfile)
+                      slice.save(os.path.join(path, dirname, outfile)
                 
 '''while ir != rows:
     while ic != cols:
