7cdeeee0a5de8f8ee661a387fb8483fb66f5bb7f
Lucid003 <68939550+Lucid003@users.noreply.github.com>
2020-10-21 16:09:45 -0500

    Update imagesplit.py
    replaced target single-file functionality with target folder functionality

--- imagesplit.py
+++ imagesplit.py
@@ -1,8 +1,12 @@
+import math
+import os
 import sys
 from PIL import Image, ImageDraw
  
 #temporary code for single use
-im = Image.open("48x48.png")
+#im = Image.open("48x48.png")
+
+
 '''entries = os.listdir('images/')
 toProcess = []
 for image in entries:
@@ -17,7 +21,28 @@ cols = im.size[1] / tilesize
 
 ir = 0
 ic = 0
-while ir != rows:
+
+with os.scandir(/') as entries:
+    for entry in entries:
+        im = Image.open(entry)
+        rows = im.size[0] / tilesize
+        rows = math.ceil(rows)
+        cols = im.size[1] / tilesize
+        cols = math.ceil(cols)
+        for x in range(rows):
+                for y in range(cols):
+                      box = (0 + ic * tilesize, 0 + ir * tilesize, tilesize + ic * tilesize, tilesize + ir * tilesize)
+                      region = im.crop(box)
+                      slice = Image.new('RGBA', (tilesize, tilesize), color=255)
+                      mask=Image.new('L', slice.size, color=255)
+                      draw=ImageDraw.Draw(mask)
+                      draw.rectangle((0,0,tilesize,tilesize), fill=0)
+                      slice.putalpha(mask)
+                      slice.paste(region, (0,0))
+                      outfile = "r" + str(ir+1) + "c" + str(ic+1) + ".png"
+                      slice.save(outfile)
+                
+'''while ir != rows:
     while ic != cols:
         box = (0 + ic * tilesize, 0 + ir * tilesize, tilesize + ic * tilesize, tilesize + ir * tilesize)
         region = im.crop(box)
@@ -31,4 +56,4 @@ while ir != rows:
         slice.save(outfile)
         ic += 1
     ir +=1
-    ic = 0
+    ic = 0'''
