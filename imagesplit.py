8eae0be9e7d9619386684a54daa31c24955179e6
Lucid003 <68939550+Lucid003@users.noreply.github.com>
2021-01-25 12:12:16 -0600

    Update imagesplit.py
    Added EasyGUI for entering image output size, and image/folder selection.

--- imagesplit.py
+++ imagesplit.py
@@ -1,26 +1,43 @@
 import math
 import os
 import sys
+from easygui import *
 from PIL import Image, ImageDraw
-
 
 while True: # basic input validation
     try:
-        tilesize = int(input('Enter your desired tile size in pixels (Example: 16 will be a 16x16 tile):'))
+        tilesize = int(enterbox("Enter your desired tile size in pixels (Example: 16 will be a 16x16 tile:"))
+        # tilesize = int(input('Enter your desired tile size in pixels (Example: 16 will be a 16x16 tile):'))
         break
     except:
         print("You must enter an integer!")
 
-folder = 'toslice/' # change this to your desired location
-toslice = os.listdir('toslice/') # gets all objects in your folder, even folders so make sure it's just images
+choices = ('Image', 'Folder', "Cancel")
+decision = buttonbox("Would you like to split an image or a folder of images?", "Image Splitter", choices)
+if decision == "Folder":
+    folder = diropenbox("", "Select a folder with images to slice...")
+    # folder = 'toslice/' # change this to your desired location
+    toslice = os.listdir(folder) # gets all objects in your folder, even folders so make sure it's just images
+elif decision == "Image":
+    toslice = []
+    toslice.append(fileopenbox("Select a file to split...", "Image Splitter"))
+elif decision == "Cancel":
+    exit()
 for entry in toslice:
-    im = Image.open(os.path.join(folder, entry)) # open image
+    dirname = entry.rstrip('.png') # folder name for output
+    dirname = dirname.rstrip('.jpg') # folder name for output
+    dirname = dirname.rstrip('.gif') # folder name for output
+    if decision == "Folder":
+        im = Image.open(os.path.join(folder, entry)) # open image
+        os.mkdir(os.path.join(folder, dirname)) # makes output folder
+    elif decision == "Image":
+        im = Image.open(entry)
+        os.mkdir(dirname) # makes output folder
     rows = im.size[1] / tilesize 
     rows = math.ceil(rows) # for images that aren't squares
     cols = im.size[0] / tilesize
     cols = math.ceil(cols) # same as above comment
-    dirname = entry.rstrip('.png') # folder name for output
-    os.mkdir(os.path.join(folder, dirname)) # makes output polder
+    # os.mkdir(os.path.join(folder, dirname)) # makes output folder
     for x in range(rows):
         for y in range(cols):
             # box is a square representing the desired tilesize at the desired tile location
@@ -33,4 +50,8 @@ for entry in toslice:
             slice.putalpha(mask) # applies alpha mask to slice
             slice.paste(region, (0,0)) # applies copied (region) image data to slice
             outfile = "r" + str(x+1) + "c" + str(y+1) + ".png" # prepping output file name
-            slice.save(os.path.join(folder, dirname, outfile)) # saving output file
+            if decision == "Folder":
+                slice.save(os.path.join(folder, dirname, outfile)) # saving output file
+            else:
+                slice.save(os.path.join(dirname, outfile)) # saving output file
+msgbox("Splitting Completed!", "Image Splitter", "End Program")
